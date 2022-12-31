b3d2a8d841cee0b392cdd9b25081624cc1819459
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2022-12-31 06:27:41 +0330

    add alias, install important pkg(apt)
    for debian base linux like ubuntu

---
+++ alias.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# author: NightFox
+
+# Update
+sudo apt update && apt -y upgrade
+
+# NightFox alias (Global)
+#
+echo "
+alias q='exit'
+alias ..='clear'
+alias py='python3'
+alias lp='echo;ls -a1FguvX;echo;pwd;echo;'
+alias srv='service --status-all'
+alias srx='service --status-all | grep "+"'
+" >> /root/.bashrc
+
+# Basic apt packages to install
+#
+# tree
+# nmap
+# net-tools(ifconfig)
+# mlocate(locate)
+# macchanger
+# htop
+# python3
+# vim
+# git
+apt -y install tree nmap net-tools mlocate macchanger htop python3 vim git
+
+# Servers
+#
+# FTP -> vsftp
+# PPTP -> pptpd
+# Apache -> apache2
+apt -y install vsftpd pptpd apache2
+
+# PHP packages
+apt -y install php libapache2-mod-php php-cli php-cgi php-mysql php-pgsql
+
+# all-in-one
+# apt -y install tree nmap net-tools mlocate macchanger htop python3 vim git vsftpd pptpd apache2 php libapache2-mod-php php-cli php-cgi php-mysql php-pgsql
+
+# end of process
+# init 6
+reboot
