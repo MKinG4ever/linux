328f28368378c19a7434eedb190170ec54c6dedf
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2022-12-31 14:40:03 +0330

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -8,6 +8,7 @@ sudo apt update && apt -y upgrade
 # NightFox alias (Global)
 #
 echo "
+# NightFox alias
 alias q='exit'
 alias ..='clear'
 alias py='python3'
@@ -39,11 +40,14 @@ apt -y install vsftpd pptpd apache2
 
 # PHP packages
 apt -y install php libapache2-mod-php php-cli php-cgi php-mysql php-pgsql
-
-# all-in-one
+ 
+# all-in-one (for easy copy)
 # apt -y install tree nmap net-tools mlocate macchanger htop python3 vim git vsftpd pptpd apache2 php libapache2-mod-php php-cli php-cgi php-mysql php-pgsql
 
+
 # End of process.
+# presentation
+echo "$USER:Done!"
 # init 6 - Reboot the machine
 # init 0 - Power-off the machine
 reboot
